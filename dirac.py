75f60348659d5f2a83ada176e6a7771bbb634e65
wecassidy <wec.notif.github@gmail.com>
2021-02-15 17:30:20 -0500

    DiracExpr is a more descriptive name than Pipe

--- dirac.py
+++ dirac.py
@@ -4,10 +4,18 @@ from sympy.core.expr import Expr
 from sympy.physics.quantum import Bra, Ket
 
 
-@dataclass
-class Pipe:
-    lhs: Expr
-    rhs: Expr
+class DiracExpr:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __gt__(self, rhs):
+        """Convert A | B > C to DiracExpr(A, |B> * C)"""
+        if self.lhs is None and rhs is None:
+            return Ket(self.rhs)
+        if rhs is None:
+            return Pipe(A, Ket(self.rhs))
+        raise TypeError
 
 
 def start_bra(old_lt, lhs, rhs):
@@ -27,6 +35,6 @@ def end_ket(old_gt, lhs, rhs):
 old_expr_le = Expr.__le__
 old_expr_gt = Expr.__gt__
 
-Expr.__le__ = lambda lhs, rhs: start_bra(old_expr_le, lhs, rhs)
-Expr.__gt__ = lambda lhs, rhs: end_ket(old_op_gt, lhs, rhs)
+# Expr.__le__ = lambda lhs, rhs: start_bra(old_expr_le, lhs, rhs)
+# Expr.__gt__ = lambda lhs, rhs: end_ket(old_op_gt, lhs, rhs)
 Expr.__or__ = lambda lhs, rhs: Pipe(lhs, rhs)
