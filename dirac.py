b708d8e71567dfd33816f1f7b2e2d0b4b15c290d
wecassidy <wec.notif.github@gmail.com>
2021-02-15 17:57:11 -0500

    SUCCCESSSSSS! UNLIMITED POWER!!!!
    Instead of overloading comparison operators, use the shift operators
    instead. These have two advantages:
    
    - `a << b` and `b >> a` are different, whereas the interpreter is allowed to
      swap `a < b` for `b > a`
    - The shift operators have higher precedence than bitwise OR, but
      lower precedence that the usual arithmetic operators. The former
      makes parsing easy (assuming valid syntax) while the latter makes
      the expressions parse as you would expect: e.g. `| k + 1 >>`
      becomes `Ket(k + 1)` (with appropriate LHS and RHS).

--- dirac.py
+++ dirac.py
@@ -4,37 +4,27 @@ from sympy.core.expr import Expr
 from sympy.physics.quantum import Bra, Ket
 
 
-class DiracExpr:
-    def __init__(self, lhs, rhs):
-        self.lhs = lhs
-        self.rhs = rhs
+@dataclass
+class BraStart:
+    lhs: Expr
+    expr: Expr
 
-    def __gt__(self, rhs):
-        """Convert A | B > C to DiracExpr(A, |B> * C)"""
-        if self.lhs is None and rhs is None:
-            return Ket(self.rhs)
-        if rhs is None:
-            return Pipe(A, Ket(self.rhs))
-        raise TypeError
+    def __or__(self, rhs):
+        if isinstance(rhs, KetEnd):
+            rhs = Ket(rhs.expr) * rhs.rhs
+        return self.lhs * Bra(self.expr) * rhs
 
 
-def start_bra(old_lt, lhs, rhs):
-    if isinstance(rhs, Pipe):
-        return lhs * Bra(rhs.lhs)
-    else:
-        return old_lt(lhs, rhs)
+@dataclass
+class KetEnd:
+    expr: Expr
+    rhs: Expr
+
+    def __ror__(self, lhs):
+        if isinstance(lhs, BraStart):
+            lhs = lhs.lhs * Bra(lhs.expr)
+        return lhs * Ket(self.expr) * self.rhs
 
 
-def end_ket(old_gt, lhs, rhs):
-    if isinstance(lhs, Pipe):
-        return Ket(lhs.rhs) * rhs
-    else:
-        return old_gt(lhs, rhs)
-
-
-old_expr_le = Expr.__le__
-old_expr_gt = Expr.__gt__
-
-# Expr.__le__ = lambda lhs, rhs: start_bra(old_expr_le, lhs, rhs)
-# Expr.__gt__ = lambda lhs, rhs: end_ket(old_op_gt, lhs, rhs)
-Expr.__or__ = lambda lhs, rhs: Pipe(lhs, rhs)
+Expr.__lshift__ = lambda lhs, rhs: BraStart(lhs, rhs)
+Expr.__rshift__ = lambda lhs, rhs: KetEnd(lhs, rhs)
