20519752102e56db94d0e915edd344307e2087df
wecassidy <wec.notif.github@gmail.com>
2021-02-15 15:01:59 -0500

    Monkeypatch expr and operator

--- dirac.py
+++ dirac.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import sympy
 from sympy.core.expr import Expr
 from sympy.physics.quantum import Bra, Ket
+from sympy.physics.quantum.operator import Operator
 
 
 @dataclass
@@ -12,7 +13,6 @@ class Pipe:
 
 
 def start_bra(old_lt, lhs, rhs):
-    print(lhs, rhs)
     if isinstance(rhs, Pipe):
         return lhs * Bra(rhs.lhs)
     else:
@@ -26,5 +26,14 @@ def end_ket(old_gt, lhs, rhs):
         return old_gt(lhs, rhs)
 
 
-def pipe(old_or, lhs, rhs):
-    return Pipe(lhs, rhs)
+old_op_le = Operator.__le__
+old_op_gt = Operator.__gt__
+
+old_expr_le = Expr.__le__
+old_expr_gt = Expr.__gt__
+
+Operator.__le__ = lambda lhs, rhs: start_bra(old_op_le, lhs, rhs)
+Operator.__gt__ = lambda lhs, rhs: end_ket(old_op_gt, lhs, rhs)
+Expr.__le__ = lambda lhs, rhs: start_bra(old_expr_le, lhs, rhs)
+Expr.__gt__ = lambda lhs, rhs: end_ket(old_op_gt, lhs, rhs)
+Expr.__or__ = lambda lhs, rhs: Pipe(lhs, rhs)
