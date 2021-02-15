1e842884ad50e97cd49d0791f2d7ea60e58b319e
wecassidy <wec.notif.github@gmail.com>
2021-02-15 15:09:13 -0500

    Don't monkey patch operators, which subclass Expr

--- dirac.py
+++ dirac.py
@@ -1,9 +1,7 @@
 from dataclasses import dataclass
 
-import sympy
 from sympy.core.expr import Expr
 from sympy.physics.quantum import Bra, Ket
-from sympy.physics.quantum.operator import Operator
 
 
 @dataclass
@@ -26,14 +24,9 @@ def end_ket(old_gt, lhs, rhs):
         return old_gt(lhs, rhs)
 
 
-old_op_le = Operator.__le__
-old_op_gt = Operator.__gt__
-
 old_expr_le = Expr.__le__
 old_expr_gt = Expr.__gt__
 
-Operator.__le__ = lambda lhs, rhs: start_bra(old_op_le, lhs, rhs)
-Operator.__gt__ = lambda lhs, rhs: end_ket(old_op_gt, lhs, rhs)
 Expr.__le__ = lambda lhs, rhs: start_bra(old_expr_le, lhs, rhs)
 Expr.__gt__ = lambda lhs, rhs: end_ket(old_op_gt, lhs, rhs)
 Expr.__or__ = lambda lhs, rhs: Pipe(lhs, rhs)
