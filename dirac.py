e767cf7c46becbb7380ea90fb29ad2e305b3c89f
wecassidy <wec.notif.github@gmail.com>
2021-02-15 11:57:46 -0500

    Make inner product work

--- dirac.py
+++ dirac.py
@@ -1,12 +1,13 @@
 import sympy
+from sympy import oo
 
 
 class Bra:
     def __init__(self, expression):
         self.expression = sympy.conjugate(expression)
 
-    def inner_product(self, ket):
-        return sympy.integrate(bra * ket.expression)
+    def inner_product(self, ket, var):
+        return sympy.integrate(self.expression * ket.expression, (var, -oo, oo))
 
 
 class Ket:
